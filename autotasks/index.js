ff5c830ab014260511be62630b87bcf8739c8a72
Stephen Webber <offgridauthor@gmail.com>
2022-06-26 18:11:25 -0500

    add sentinel client functionality

--- autotasks/index.js
+++ autotasks/index.js
@@ -1,12 +1,16 @@
-const { AdminClient } = require('defender-admin-client');
+const { AdminClient } = require('defender-admin-client')
+const { SentinelClient } = require('defender-sentinel-client')
 
-exports.handler = async function(event) {
-  const credentials = {apiKey: event.secrets.API_KEY, apiSecret: event.secrets.API_SECRET};
-  const client = new AdminClient(credentials); 
-   
-  const payload = event.request.body;
-  const matchReasons = payload.matchReasons;
-  const newCloneAddress = matchReasons[0].params._clone;
+exports.handler = async function (event) {
+  const credentials = {
+    apiKey: event.secrets.API_KEY,
+    apiSecret: event.secrets.API_SECRET,
+  }
+  const adminClient = new AdminClient(credentials)
+  const sentinelClient = new SentinelClient(credentials)
+  const payload = event.request.body
+  const matchReasons = payload.matchReasons
+  const newCloneAddress = matchReasons[0].params._clone
   const newCloneAbi = `[
     {
       "anonymous": false,
@@ -60,14 +64,18 @@ exports.handler = async function(event) {
       "stateMutability": "nonpayable",
       "type": "function"
     }
-  ]`;
-
-	await client.addContract({ 
-		network: 'rinkeby', 
-		address: newCloneAddress, 
-		name: `Clone ${newCloneAddress}`,  
-		abi: newCloneAbi,
-	});
-
-
+  ]`
+  // add new box clone contract to admin
+  await adminClient.addContract({
+    network: 'rinkeby',
+    address: newCloneAddress,
+    name: `Clone ${newCloneAddress}`,
+    abi: newCloneAbi,
+  })
+  // add new box clone to sentinel
+  const subscriberId = '1cf990de-ebb3-4255-8c12-67eec8fbbfa7'
+  const sentinel = await sentinelClient.get(subscriberId)
+  const subscribedAddresses = sentinel.addressRules[0].addresses
+  subscribedAddresses.push(newCloneAddress)
+  await sentinelClient.update(subscriberId, { addresses: subscribedAddresses })
 }
